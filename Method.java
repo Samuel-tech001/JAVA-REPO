b2ccf381a99aaf13107b3decfdb3a11020b6bb94
Samuelkg <158509298+Samuel-tech001@users.noreply.github.com>
2025-02-06 23:20:35 -0800

    Delete Method.java

--- Method.java
+++
@@ -1,8 +0,0 @@
-public class Method{
-    static void myMethod(){
-        System.out.println("I just got executed");
-    }
-    public static void main(String[]args){
-        myMethod();
-    }
-}
